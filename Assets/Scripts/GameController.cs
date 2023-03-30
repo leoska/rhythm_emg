27fa1d25a6ac402067290ae142cafb0b7bd247ec
leoska <leo77551@gmail.com>
2023-03-30 23:35:41 +0300

    complete mechanic first 6 notes

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,12 +1,15 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    
-    private static GameController _gameController = null;
+    private NoteActivator _redActivator = null;
+    private NoteActivator _yellowActivator = null;
+    private NoteActivator _greenActivator = null;
+
+    [Header("Score & Rock")] 
+    public int score = 0;
+    public int rock = 5;
     
     [Header("Note speed")] 
     [Range(5f, 25f)]
@@ -21,16 +24,12 @@ public class GameController : MonoBehaviour
     public KeyCode yellowActivatorButton = KeyCode.X;
     public KeyCode greenActivatorButton = KeyCode.C;
     
-    public static GameController Instance 
-    {
-        get => _gameController;
-        private set => _gameController = value;
-    }
+    public static GameController Instance { get; private set; } = null;
 
 
     private void Awake()
     {
-        _gameController = this;
+        Instance = this;
     }
 
     // Start is called before the first frame update
@@ -42,6 +41,102 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        var isActivateRed = Input.GetKeyDown(redActivatorButton);
+        var isActivateYellow = Input.GetKeyDown(yellowActivatorButton);
+        var isActivateGreen = Input.GetKeyDown(greenActivatorButton);
+
+        if (isActivateRed)
+        {
+            _TryActivate(Notes.Red);
+        } 
+        else if (isActivateYellow)
+        {
+            _TryActivate(Notes.Yellow);
+        } 
+        else if (isActivateGreen)
+        {
+            _TryActivate(Notes.Green);
+        }
+    }
+
+    private void _TryActivate(Notes noteType)
+    {
+        var isFail = true;
+        var addScore = 100;
+        var addRock = 1;
         
+        switch (noteType)
+        {
+            case Notes.Red:
+                if (_redActivator != null)
+                {
+                    isFail = false;
+                    _redActivator.Activate();
+                    _redActivator = null;
+                }
+                break;
+
+            case Notes.Yellow:
+                if (_yellowActivator != null)
+                {
+                        isFail = false;
+                        _yellowActivator.Activate();
+                        _yellowActivator = null;
+                } 
+                break;
+            
+            case Notes.Green:
+                if (_greenActivator != null)
+                {
+                    isFail = false;
+                    _greenActivator.Activate();
+                    _greenActivator = null;
+                }
+                break;
+            
+            default:
+                throw new ArgumentOutOfRangeException(nameof(noteType), noteType, null);
+        }
+        
+        if (isFail)
+        {
+            rock -= addRock;
+        }
+        else
+        {
+            score += addScore;
+            rock += addScore;
+        }
+    }
+
+    public void NoteCanActivateRed(NoteActivator noteActivator)
+    {
+        if (_redActivator != null)
+            throw new Exception("Previous Red Note Can Activate!");
+
+        _redActivator = noteActivator;
+    }
+    
+    public void NoteCanActivateYellow(NoteActivator noteActivator)
+    {
+        if (_yellowActivator != null)
+            throw new Exception("Previous Red Note Can Activate!");
+
+        _yellowActivator = noteActivator;
+    }
+    
+    public void NoteCanActivateGreen(NoteActivator noteActivator)
+    {
+        if (_greenActivator != null)
+            throw new Exception("Previous Red Note Can Activate!");
+
+        _greenActivator = noteActivator;
+    }
+
+    public enum Notes
+    {
+        Red = 0,
+        Yellow = 1,
+        Green = 2,
     }
 }
--- Assets/Scripts/NoteActivator.cs
+++ Assets/Scripts/NoteActivator.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NoteActivator : MonoBehaviour
 {
+
+    public GameController.Notes noteType = GameController.Notes.Red;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,51 @@ public class NoteActivator : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var gameController = GameController.Instance;
+        
+        if (other.gameObject.CompareTag("Activator Trigger"))
+        {
+            switch (noteType)
+            {
+                case GameController.Notes.Red:
+                    gameController.NoteCanActivateRed(this);
+                    break;
+                
+                case GameController.Notes.Yellow:
+                    gameController.NoteCanActivateYellow(this);
+                    break;
+                
+                case GameController.Notes.Green:
+                    gameController.NoteCanActivateGreen(this);
+                    break;
+                
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(noteType), noteType, null);
+            }
+        }
+        
+        Debug.Log(other.gameObject.name);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Activator Trigger"))
+        {
+            Fail();
+        }
+    }
+
+    public void Activate()
+    {
+        Destroy(gameObject.transform.parent.gameObject);
+    }
+
+    private void Fail()
+    {
+        Destroy(gameObject.transform.parent.gameObject);
     }
 }
